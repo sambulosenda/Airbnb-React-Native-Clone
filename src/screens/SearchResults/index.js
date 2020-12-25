d340c79935133916fe0b4cc348dfddd83427a04e
Sambulo Senda <admin@users-MacBook-Air.local>
2020-12-25 18:06:51 +0000

    posts loading from grapQL api

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { StyleSheet, Text, View, FlatList} from 'react-native'
 
 import feed from '../../assets/data/feed'
 import Post from '../../components/Post'
 
+import {API, graphqlOperation } from 'aws-amplify';
+import {listPosts} from '../../graphql/queries'
+
 const SearchResultsScreen = () => {
+
+const [posts, setPosts] = useState([])
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const postResult = await API.graphql(
+                    graphqlOperation(listPosts)
+                )
+                setPosts(postResult.data.listPosts.items);
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+       fetchPosts();
+    }, [])
+
     return (
         <View>
             <FlatList 
-            data={feed}
+            data={posts}
             renderItem={({item}) => <Post post={item} />}
             />
         </View>
